3e48cb4d111f3193e6afd420f630fc73b412e92c
Alex Xandra Albert Sim <bertzzie@gmail.com>
2015-08-16 21:16:32 +0700

    Frontpage initial design. Flawed

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/revel/revel"
+	"obrolansubuh.com/models"
 	osc "obrolansubuh.com/modules/gorm/app/controllers"
 )
 
@@ -10,5 +11,10 @@ type App struct {
 }
 
 func (c App) Index() revel.Result {
-	return c.Render()
+	var posts []models.Post
+	c.Trx.Limit(5).Order("created_at desc").
+		Where("published = true").
+		Find(&posts)
+
+	return c.Render(posts)
 }
